271be2e86025e35f660aa5d7f62df0f7acc818b6
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-10-15 16:40:17 -0400

    Create the user class

--- primer_proyecto/main.cpp
+++ primer_proyecto/main.cpp
@@ -36,13 +36,60 @@ class Mascota
 		char *getTipo() { return animal; }
 		
         int  getEdad() { return edad; }
-        void imprimir(){ cout << nom << " " << edad << " " << animal << endl; }
+        void mostrar(){ cout << nom << " " << edad << " " << animal << endl; }
+};
+
+
+class Usuario 
+{
+	private:
+		char cedula[15];
+		char nombre[30];
+		char estado[30];
+		char ciudad[30];
+		char direccion[30];
+		char telefono[30];
+		int edad;
+		bool esApto = true;
+	
+	public: 
+	
+		Usuario() { }
+		Usuario(char *ci, char *n, char *e, char *c, char *d, char *t, int edad){
+			strcpy(cedula, ci);
+			strcpy(nombre, n);
+			strcpy(estado, e);
+			strcpy(ciudad, c);
+			strcpy(direccion, d);
+			strcpy(telefono, t);
+			this->edad = edad;
+		}
+		
+		char *getRef(){
+			return cedula;
+		}
+		
+		void mostrar(){ 
+			cout << nombre << " " << edad << " " << telefono << endl;
+		}
+		
+		void setEsApto(bool esApto){
+			this->esApto = esApto;
+		}
+		
+		bool getEsApto(){
+			return esApto;
+		}
 };
 
 int main() {
 
 	Mascota a("Manchas", 5, "callejero", "5487L", "De la unet", 'F', "perro");
-    a.imprimir();
+	Usuario u("V26493551", "Jefferson", "Tachira", "Tariba", "Calle 3", "+58 412 54602929", 22);
+	
+    a.mostrar();
+    u.mostrar();
+    
     
     return 0;
 }
