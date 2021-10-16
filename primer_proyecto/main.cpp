3db045934305d59d474191078e32b017ed8fac35
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-10-16 00:03:11 -0400

    Create the functions to display

--- primer_proyecto/main.cpp
+++ primer_proyecto/main.cpp
@@ -217,7 +217,7 @@ void insertarUsuario(){
 }
 
 
-void ingresarAdopcion(){
+void insertarAdopcion(){
 	char cedula[30];
 	char codigo[30];
 	char fecha[15];
@@ -242,57 +242,38 @@ void ingresarAdopcion(){
 	}else
 		cout << endl << endl << "Ha ocurrido un error :(" << endl << endl;
 	
-		
-	
 	system("pause");
 }
 
-int main() {
 
-	Mascota a("Manchas", 5, "callejero", "5487L", "De la unet", 'F', "perro");
-	Mascota a1("Coqui", 8, "Labrador", "5498CM", "De la unet", 'M', "perro");
-	Mascota a2("Wilson", 7, "Gato rubio", "8789LO", "De noruega", 'M', "gato");
-	
-	Usuario u("V26493551", "Jefferson", "Tachira", "Tariba", "Calle 3", "+58 412 54602929", 22);
-	Usuario u1("V89754211", "Pedro", "Tachira", "Michelena", "Calle 8", "+58 424 59848578", 25);
-	Usuario u2("E98785485", "Milagros", "Norte de Santader", "Cucuta", "Calle 7", "+58 412 54602929", 23);
-	
-	Adopcion adop("V26493551", "5498CM", "27/10/21");
-	Adopcion adop1("E98785485", "5487L", "13/11/21");
-	
-    
-    cout  << endl << endl << "numero de registros " << ++numeroDeRegistros << endl << endl;
-	
-	cout << "Archivos:" << endl;
-	cout << archivoMascota.get_nomb() << endl;
-	cout << archivoUsuario.get_nomb() << endl;
-	cout << archivoAdopcion.get_nomb() << endl;
-	
-	cout << "Insertando datos a los archivos..." << endl;
-	archivoMascota.insertar(a);
-	archivoMascota.insertar(a1);
-	archivoMascota.insertar(a2);
-	archivoUsuario.insertar(u);
-	archivoUsuario.insertar(u1);
-	archivoUsuario.insertar(u2);
-	archivoAdopcion.insertar(adop);
-	archivoAdopcion.insertar(adop1);
-	cout << endl << endl;
-	
-	cout << "Listado de mascotas:" << endl;
+void listarMascotas(){
 	archivoMascota.listar();
 	cout << endl << endl;
-	
-	cout << "Listado de Usuarios" << endl;
+}
+
+
+void listarUsuarios(){
 	archivoUsuario.listar();
 	cout << endl << endl;
-	
-	cout << "Listado de Adopciones" << endl;
+}
+
+
+void listarAdopciones(){
 	archivoAdopcion.listar();
 	cout << endl << endl;
+}
 
 
+int main() {
 	
+	insertarMascota();
+	insertarUsuario();
+	insertarAdopcion();
+	
+	listarMascotas();
+	listarUsuarios();
+	listarAdopciones();
+
 	cout << endl;
     system("pause");
     return EXIT_SUCCESS;
