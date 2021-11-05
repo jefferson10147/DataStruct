c26123cca6c8f7739afbfc7696d57ea83d05e9ba
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-11-05 00:43:44 -0400

    Create the necessary data structures

--- segundo_parcial_proyecto_1/main.cpp
+++ segundo_parcial_proyecto_1/main.cpp
@@ -1,23 +1,47 @@
 #include <iostream>
 
 #include "ListaEnlazada.h"
+#include "Colas_dinamicas.h"
+#include "Bicola.h"
+#include "Pilas_dinamicas.h"
 
 using namespace std;
 
-ListaEnlazada<int> miLista;
+ListaEnlazada<char*> miLista;
+Colas_dinamicas<char*> colaDinamica;
+Bicola<char> bicolaPalabras;
+Pilas_dinamicas<char*> pilaDinamica;
 
 int main(int argc, char** argv) {
-	cout << "init..." << endl;
-	miLista.Anadir(5);
-	miLista.Anadir(4);
-	miLista.Anadir(3);
+	// miLista.Anadir("hola");
+	// miLista.Anadir("chao");
+	// miLista.Imprimir();
 	
-	miLista.Imprimir();
-	
+	/* COLAS
+	colaDinamica.Anadir("holaa");
+	colaDinamica.Anadir("chao");
+	cout << colaDinamica.Leer();
 	cout << endl;
-	int valor = 4;
-	miLista.Eliminar(valor);
-	cout << "El valor es " << valor << endl;
-	miLista.Imprimir();
+	cout << colaDinamica.Leer();
+	cout << "imprimeindo la cola jeje" << endl;
+	colaDinamica.Imprimir();
+	*/
+	
+	/* BICOLA PARA LAS PALABRAS
+	bicolaPalabras.push_front('a');
+	bicolaPalabras.push_front('b');
+	bicolaPalabras.push_front('c');
+	
+	cout << bicolaPalabras.pop_front() << endl;
+	cout << bicolaPalabras.pop_front() << endl;
+	cout << bicolaPalabras.pop_front() << endl;
+	*/
+	
+	pilaDinamica.Anadir("primero en la pila, deberia ser el ultimo en salir");
+	pilaDinamica.Anadir("segundo en la pila, deberia ser el segundo en salir");
+	pilaDinamica.Anadir("ultimo en la pila, deberia ser el primero en salir");
+	cout << pilaDinamica.Leer() << endl;
+	cout << pilaDinamica.Leer() << endl;
+	cout << pilaDinamica.Leer() << endl;
 	return 0;
 }
