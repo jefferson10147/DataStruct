83978f9d1a7f1884cee9336efcf9e1546b3b2517
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-11-10 18:52:19 -0400

    Create the function to read the file

--- tercer_parcial_proyecto_1/main.cpp
+++ tercer_parcial_proyecto_1/main.cpp
@@ -1,9 +1,47 @@
+/*
+	Realizado por: Jefferson S. Montilla Mendoza C.I 26493551
+	Tercer Proyecto
+	Estructuras de datos
+*/
+
 #include <iostream>
+#include <cstring>
+#include <fstream>
+
+#include "ArbolBB.h"
 
 using namespace std;
 
+void cargarArboles(ArbolBB<string> ListaArbolitos[]);
+
 int main(int argc, char** argv) {
-	cout << "Hello world" << endl;
+	
+	int cantidadArbolitos = 10;
+	ArbolBB<string> listaArbolitos[cantidadArbolitos];
+	
+	cargarArboles(listaArbolitos);
 	
 	return 0;
+}
+
+void cargarArboles(ArbolBB<string> listaArbolitos[]) {
+	fstream file("arbol.txt", ios::in);
+	string line;
+	char *split;
+	int i = 0;
+	
+	if (file.fail())
+		cout << "Error al abrir el archivo arbol.txt" << endl;
+	else {
+		while (getline(file,line)) {
+			split = strtok(&line[0], " ");
+			while (split) {
+				// ListaArbolitos[i].Anadir(atoi(split));
+				cout << split << endl;
+				split = strtok(NULL, " ");	
+			}
+			i++;
+		}
+	}
+	
 }
